c1e42a80256321111ae158487f7abb9a1118f650
glenr4 <glenrutherford4@gmail.com>
2016-02-16 14:53:21 +0800

    Controller iniating input values working

---
+++ js/app.js
@@ -0,0 +1,12 @@
+angular.module('myApp', [])
+	.controller('myCtrl', function($scope){
+		$scope.maleName = 'Male name';
+		$scope.dirtyTask = 'dirty task';
+		$scope.obnxCeleb = 'obnoxious celebrity'; 
+		$scope.jobTitle = 'Job Title'; 
+		$scope.celebrity = 'celebrity'; 
+		$scope.hugeNumber = 'huge number';
+		$scope.tediousTask = 'tedious task';
+		$scope.uselessSkill = 'useless skill'; 
+		$scope.adjective = 'adjective'; 
+	});
