4d14e9e1a09b867351115cc9a3c24ba7b0c04dc1
glenr4 <glenrutherford4@gmail.com>
2016-02-28 09:38:16 +0800

    Functions correctly

--- js/app.js
+++ js/app.js
@@ -32,10 +32,48 @@ angular.module('myApp', ['ngMessages'])
 
 		};
 
+		// Reset form
+		$scope.reset = function(){
+			console.log("reset!");
+
+			// Clear input values
+
+			// This method does not update the screen, why?
+			// console.log($scope.userForm.maleName.$viewValue);
+			// $scope.userForm = angular.copy(blankForm);
+			// console.log($scope.userForm.maleName.$viewValue);
+
+			// THere should be a better way than this!
+			$scope.maleName = "";
+			$scope.dirtyTask = "";
+			$scope.obnxCeleb = "";
+			$scope.jobTitle = "";
+			$scope.celebrity = "";
+			$scope.hugeNumber = "";
+			$scope.tediousTask = "";
+			$scope.uselessSkill = "";
+			$scope.adjective = "";
+
+			$scope.userForm.$setPristine();
+			$scope.userForm.$setUntouched();
+
+			// Show input form again
+			$scope.inputValid = "";
+
+				
+
+		};
+
 
 		// Initialise variables
 		$scope.maleFemale();
 		$scope.inputValid = "";
+		var blankForm;
+
+		// This is only used for the angular.copy reset method which does not work
+		angular.element(document).ready(function(){
+			blankForm = angular.copy($scope.userForm);
+		});
 	});
 
 
